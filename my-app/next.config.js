47a4c6ff085fbfc75b338d5a48b7bb8e68f266bd
Krishna <krishnasumagaysay550@gmail.com>
2026-03-10 23:37:16 +0800

    Prepare for Render deployment

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    turbo: false,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: { turbo: false },
+  outputFileTracingRoot: undefined, // explicitly unset
 };
-
 module.exports = nextConfig;
