d58606573813355b7092778d9ad81b7f5234c4e1
Krishna <krishnasumagaysay550@gmail.com>
2026-03-14 01:14:15 +0800

    Add CareerTest page with iframe to Streamlit app

--- my-app/src/app/career-test/page.tsx
+++ my-app/src/app/career-test/page.tsx
@@ -4,7 +4,7 @@ export default function CareerTest() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="http://localhost:8501"
+        src="https://career-test.streamlit.app/"
         style={{ width: "100%", height: "100%", border: "none" }}
         title="Career Test"
       />
