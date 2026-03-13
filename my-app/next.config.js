9ce9653b95fec600f8b3d873366d9dbf4ad53b6d
Krishna <krishnasumagaysay550@gmail.com>
2026-03-14 01:34:20 +0800

    Allow embedding Streamlit app in iframe

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,23 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   experimental: { turbo: false },
   outputFileTracingRoot: undefined, // explicitly unset
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://career-test.streamlit.app",
+          },
+        ],
+      },
+    ];
+  },
 };
+
 module.exports = nextConfig;
