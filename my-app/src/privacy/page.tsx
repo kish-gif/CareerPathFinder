8c5be92690b4895c56063849419c4dcc03299397
Krishna <krishnasumagaysay550@gmail.com>
2026-03-14 16:26:28 +0800

    Add Terms and Privacy pages

---
+++ my-app/src/privacy/page.tsx
@@ -0,0 +1,9 @@
+// src/app/privacy/page.tsx
+export default function PrivacyPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Privacy Policy</h1>
+      <p>Here is how we handle your data...</p>
+    </div>
+  );
+}
