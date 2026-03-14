592a0937faa7caf8cc952001faf803040e8c7488
Krishna <krishnasumagaysay550@gmail.com>
2026-03-14 18:01:15 +0800

    Add Terms and Privacy pages

---
+++ my-app/src/app/terms/page.tsx
@@ -0,0 +1,9 @@
+// src/app/terms/page.tsx
+export default function TermsPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Terms of Service</h1>
+      <p>Here are the terms of using Career Path Finder...</p>
+    </div>
+  );
+}
